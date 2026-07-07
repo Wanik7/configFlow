5cb8094e87a6a51b0b9a0b620e9aa6ac712285a2
Wanik7 <wanikuteikin@mail.ru>
2026-07-07 16:11:53 +0300

    feat: implement parallel backup processing with worker pool (engine.go) and add project gitignore rules

--- engine.go
+++ engine.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"sync"
 )
 
 type SyncEngine struct {
@@ -57,6 +58,71 @@ func (se *SyncEngine) Restore(backupFile, destFile, storageName string) error {
 	}
 
 	return nil
+}
+
+type BackupJob struct {
+	SourceData  string
+	BackupDest  string
+	StorageType string // archive or secure
+}
+
+func (se *SyncEngine) worker(id uint, jobs <-chan BackupJob, results chan<- string, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for job := range jobs {
+		storage, ok := se.storages[job.StorageType]
+		if !ok {
+			results <- fmt.Sprintf("[Worker %d] No such storage: %s", id, job.StorageType)
+			continue
+		}
+
+		targetFile, err := os.Open(job.SourceData)
+		if err != nil {
+			results <- fmt.Sprintf("[Worker %d] Could not open target file: %v", id, err)
+			continue
+		}
+
+		err = storage.Store(job.BackupDest, targetFile)
+		CloseSafe(targetFile)
+		if err != nil {
+			results <- fmt.Sprintf("[Worker %d] Could not backup target file: %v", id, err)
+			continue
+		} else {
+			results <- fmt.Sprintf("[Worker %d] Successfully backed up %s to %s [%s]",
+				id, job.SourceData, job.BackupDest, job.StorageType)
+		}
+	}
+}
+
+func (se *SyncEngine) ParallelBackup(jobList []BackupJob, maxWorkers int) {
+	jobsChan := make(chan BackupJob, len(jobList))
+	results := make(chan string, len(jobList))
+
+	var wg sync.WaitGroup
+
+	for id := 1; id <= maxWorkers; id++ {
+		wg.Add(1)
+		go se.worker(uint(id), jobsChan, results, &wg)
+	}
+
+	for _, job := range jobList {
+		jobsChan <- job
+	}
+
+	close(jobsChan)
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	fmt.Printf("--- Launch %d workers for %d jobs ---\n", maxWorkers, len(jobList))
+
+	for res := range results {
+		fmt.Println(res)
+	}
+
+	fmt.Println("\n--- Done ---")
 }
 
 func CloseSafe(c io.Closer) {
